9597f455d69094aa7718d31c2743cb612eb77c2b
santhoshn0905 <santhoshn0905@gmail.com>
2025-01-28 10:51:50 +0530

    Sathish

--- app/src/main/java/com/example/annauniversity/MainActivity.java
+++ app/src/main/java/com/example/annauniversity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.annauniversity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +9,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView dashboard_icon_bgImg=findViewById(R.id.dashboardtheme_1);
+        Glide.with(this).asGif().load(R.drawable.dashboard_theme_gif).into(dashboard_icon_bgImg);
+
     }
 }
